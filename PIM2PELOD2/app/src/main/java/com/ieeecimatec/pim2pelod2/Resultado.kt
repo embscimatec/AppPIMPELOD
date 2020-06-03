a6294bde01efd8a075530aac0578f644e9e44226
fernandanlisboa <fernandanlisboa@gmail.com>
2020-06-03 10:27:09 -0300

    formatação da apresentação do resultado

--- PIM2PELOD2/app/src/main/java/com/ieeecimatec/pim2pelod2/Resultado.kt
+++ PIM2PELOD2/app/src/main/java/com/ieeecimatec/pim2pelod2/Resultado.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_resultado.*
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 class Resultado : AppCompatActivity() {
 
@@ -12,7 +14,11 @@ class Resultado : AppCompatActivity() {
         setContentView(R.layout.activity_resultado)
 
         val resultado : Double = intent.getSerializableExtra("resultado") as Double
+        val df = DecimalFormat("##.##")
+        df.roundingMode = RoundingMode.CEILING
 
-        textView.text = "${resultado.toString()}%"
+        val resultadoStr = df.format(resultado).toString()
+
+        textView.text = "${resultadoStr}%"
     }
 }
