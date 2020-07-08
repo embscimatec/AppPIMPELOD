6ce6404d87e69be26ee42278f966c72802f7209e
fernandanlisboa <fernandanlisboa@gmail.com>
2020-07-08 14:57:10 -0300

    adição do splash calculando

---
+++ atualizado/PIM2PELOD22/app/src/main/java/com/ieeecimatec/pim2pelod2/splash_calculando.kt
@@ -0,0 +1,25 @@
+package com.ieeecimatec.pim2pelod2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class splash_calculando : AppCompatActivity() {
+
+    private val SLPASH_TIME_OUT:Long = 6000 //equivale a 2s
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_calculando)
+
+        val resultado : Double = intent.getSerializableExtra("resultado") as Double
+
+        Handler().postDelayed({
+            val it = Intent(this, Resultado::class.java)
+            it.putExtra("resultado", resultado)
+            startActivity(it)
+
+            finish()
+        }, SLPASH_TIME_OUT)
+    }
+}
