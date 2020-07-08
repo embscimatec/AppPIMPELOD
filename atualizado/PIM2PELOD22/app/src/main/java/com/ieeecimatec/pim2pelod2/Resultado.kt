b9efcb734ea15016975cb92afa63f46b5a47d9c3
fernandanlisboa <fernandanlisboa@gmail.com>
2020-07-08 14:56:49 -0300

    adição do resultado

---
+++ atualizado/PIM2PELOD22/app/src/main/java/com/ieeecimatec/pim2pelod2/Resultado.kt
@@ -0,0 +1,23 @@
+package com.ieeecimatec.pim2pelod2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_resultado.*
+import java.math.RoundingMode
+import java.text.DecimalFormat
+
+class Resultado : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado)
+
+        val resultado : Double = intent.getSerializableExtra("resultado") as Double
+        val df = DecimalFormat("##.##")
+        df.roundingMode = RoundingMode.CEILING
+
+        val resultadoStr = df.format(resultado).toString()
+
+        textView.text = "${resultadoStr}%"
+    }
+}
