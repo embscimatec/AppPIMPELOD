efa4fcd09763b8a72c76fcd9b5bcfbb39b02bc09
fernandanlisboa <fernandanlisboa@gmail.com>
2020-10-17 13:09:58 -0300

    adicionando nav bottom
    Co-Authored-By: Maria Antônia Amado <48838678+AmadoMaria@users.noreply.github.com>
    Co-Authored-By: Antônio César <antonio.contato.eng@gmail.com>

--- PIMPELOD/app/src/main/java/com/ieeecimatec/pimpelod/MainActivity.kt
+++ PIMPELOD/app/src/main/java/com/ieeecimatec/pimpelod/MainActivity.kt
@@ -1,23 +1,43 @@
 package com.ieeecimatec.pimpelod
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+    BottomNavigationView.OnNavigationItemSelectedListener,
+    BottomNavigationView.OnNavigationItemReselectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        initBottomNavigation()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun clickPIM (view : View){ //esse método nao está funcionando
+    private fun initBottomNavigation() {
+        bottom_nav_view.setOnNavigationItemSelectedListener( this )
+        bottom_nav_view.setOnNavigationItemReselectedListener( this )
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        /* TODO */
+        return true
+    }
+
+    override fun onNavigationItemReselected(item: MenuItem) {
+        /* TODO */
+    }
+
+    fun clickPIM (view : View){
         val intent = Intent (this, Ficha_paciente_Pim::class.java )
         startActivity(intent)
     }
 
-    fun clickPELOD (view : View){ //esse método nao está funcionando
+    fun clickPELOD (view : View){
         val intent = Intent (this, Ficha_paciente_pelod::class.java )
         startActivity(intent)
     }
